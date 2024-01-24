edaced044248faa74a461e0312a22a614664de29
Amit Altman <44475813+AltyMan@users.noreply.github.com>
2024-01-24 15:34:12 -0500

    Update ELEC292_Lab1.py
    Added comments for Q1

--- Lab1/ELEC292_Lab1.py
+++ Lab1/ELEC292_Lab1.py
@@ -4,6 +4,7 @@ import numpy as np
 list_1 = [-1, 2, 3, 9, 0]
 list_2 = [1, 2, 7, 10, 14]
 print(list_1 + list_2)
+# The + operator combines two lists together, adding the second set of values after the first
 
 # Question 2
 list_1 = [-1, 2, 3, 9, 0]
