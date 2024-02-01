edbc68fdc603605d5c46dfdf79158aff13f60f2a
Supercobra27 <silverbergryan@gmail.com>
2024-01-31 22:16:47 -0500

    fix spelling

--- Lab2/ELEC292_Lab2.py
+++ Lab2/ELEC292_Lab2.py
@@ -13,7 +13,7 @@ for day in weather_data: #iterate over
     max_temp = span_tags[0].text.replace('°','') #get max and get rid of degree
     min_temp = span_tags[3].text.replace('°','') #get min and get rid of degree
     weather_condition = day.find('div', class_="DetailsSummary--condition--2JmHb").span.text #get condition
-    precip_chance = day.find('div', class_="DetailsSummary--precip--1a98O").span.text #get rain chaine
+    precip_chance = day.find('div', class_="DetailsSummary--precip--1a98O").span.text #get rain chance
     wind_section = day.find('div', class_="DetailsSummary--wind--1tv7t DetailsSummary--extendedData--307Ax").span.text.split() #get wind data
     final_data = (date, max_temp, min_temp, weather_condition, precip_chance, wind_section[0], wind_section[1]) #consolidate
     with open('ELEC292_Lab2.md', 'a') as f: #Open file
