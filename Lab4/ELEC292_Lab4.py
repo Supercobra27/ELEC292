8582a3b6c2c9ecc4fe5d965da559195278b34044
Supercobra27 <silverbergryan@gmail.com>
2024-03-07 22:38:11 -0500

    finished

--- Lab4/ELEC292_Lab4.py
+++ Lab4/ELEC292_Lab4.py
@@ -4,95 +4,104 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-
+from sklearn.preprocessing import StandardScaler
 
 # Question 1
-dataset = pd.read_csv("heart.csv")
-data = dataset.iloc[:, :-1]  
-labels = dataset.iloc[:, -1]
-fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
-
-data.hist(ax=ax.flatten()[:13])
-
-# Question 2
-
-
+def Q1():
+    dataset = pd.read_csv("heart.csv")
+    data = dataset.iloc[:, :-1]  
+    labels = dataset.iloc[:, -1]
+    fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
+    data.hist(ax=ax.flatten()[:13])
 
 # Question 3
-dataset = pd.read_csv("heart.csv")
-data = dataset.iloc[:, :-1]  
-labels = dataset.iloc[:, -1]
-fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
-for i in range(0, 13):
-    ax.flatten()[i].hist(data.iloc[:, i])
-    ax.flatten()[i].set_title(data.columns[i], fontsize=15)
+def Q3():
+    dataset = pd.read_csv("heart.csv")
+    data = dataset.iloc[:, :-1]  
+    labels = dataset.iloc[:, -1]
+    fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
+    for i in range(0, 13):
+        ax.flatten()[i].hist(data.iloc[:, i])
+        ax.flatten()[i].set_title(data.columns[i], fontsize=15)
 
 # Question 4
-dataset = pd.read_csv("heart.csv")
-data = dataset.iloc[:, :-1]  
-labels = dataset.iloc[:, -1]
-fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
-data.plot(ax=ax.flatten()[:13], kind='density', subplots=True, sharex=False)
+def Q4():
+    dataset = pd.read_csv("heart.csv")
+    data = dataset.iloc[:, :-1]  
+    labels = dataset.iloc[:, -1]
+    fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
+    data.plot(ax=ax.flatten()[:13], kind='density', subplots=True, sharex=False)
 
 # Question 5
-dataset = pd.read_csv("heart.csv")
-data = dataset.iloc[:, :-1]  
-labels = dataset.iloc[:, -1]
-fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
-data.plot(ax=ax.flatten()[:13], kind='box', subplots=True, sharex=False, sharey=False)
+def Q5():
+    dataset = pd.read_csv("heart.csv")
+    data = dataset.iloc[:, :-1]  
+    labels = dataset.iloc[:, -1]
+    fig, ax = plt.subplots(ncols=4, nrows=4, figsize=(20, 10))
+    data.plot(ax=ax.flatten()[:13], kind='box', subplots=True, sharex=False, sharey=False)
 
 # Question 6
-dataset = pd.read_csv("heart.csv")
-data = dataset.iloc[:, :-1]  
-labels = dataset.iloc[:, -1]
-fig, ax = plt.subplots(ncols=13, nrows=13, figsize=(30, 30))
-pd.plotting.scatter_matrix(data, ax=ax)
-
-# Question 7
+def Q6():
+    dataset = pd.read_csv("heart.csv")
+    data = dataset.iloc[:, :-1]  
+    labels = dataset.iloc[:, -1]
+    fig, ax = plt.subplots(ncols=13, nrows=13, figsize=(30, 30))
+    pd.plotting.scatter_matrix(data, ax=ax)
 
 # Question 8
-dataset = pd.read_csv("wine.csv")
-dataset = dataset.drop(dataset.columns[0], axis=1)
 
-def map_quality(quality):
-    if quality >= 8:
-        return 1
-    else:
-        return 0
+def Q89():
+    dataset = pd.read_csv("wine.csv")
+    dataset = dataset.drop(dataset.columns[0], axis=1)
 
-# Apply the function to the 'quality' column
-dataset['quality'] = dataset['quality'].apply(map_quality)
-def map_quality_to_color(quality):
-    if quality == 1:
-        return 'red'  # High-quality wines
-    else:
-        return 'pink'  # Low-quality wines
+    dataset['quality'] = dataset['quality'].apply(lambda x: 1 if x>= 8 else 0)
 
-# Apply PCA
-pca = PCA(n_components=2)
-pca_result = pca.fit_transform(dataset.drop('quality', axis=1))
+    dataset = dataset.sort_values(by='quality')
+    data = dataset.iloc[:, :-1]
+    labels = dataset.iloc[:, -1]
+    sc = StandardScaler()
+    pca = PCA(n_components=2)
+    data = sc.fit_transform(data)
+    data = pca.fit_transform(data)
 
-# Plot PCA results
-plt.figure(figsize=(8, 6))
-plt.scatter(pca_result[:, 0], pca_result[:, 1], c=dataset['quality'].apply(map_quality_to_color))
-plt.title('PCA')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
+    plt.figure(figsize=(10, 10))
+    plt.scatter(data[:, 0], data[:, 1], c=dataset['quality'].apply(lambda x: 'red' if x==1 else 'pink'))
+    plt.title('PCA 1v2')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+
+    data = dataset.iloc[:, :-1]
+    labels = dataset.iloc[:, -1]
+    data = sc.fit_transform(data)
+    tsne = TSNE(n_components=2, perplexity=30)
+    data = tsne.fit_transform(data)
+
+    plt.figure(figsize=(10,10))
+    plt.scatter(data[:, 0], data[:, 1], c=dataset['quality'].apply(lambda x: 'red' if x==1 else 'pink'))
+    plt.title('t-SNE 1v2')
+    plt.xlabel('t-SNE Component 1')
+    plt.ylabel('t-SNE Component 2')
+
+    data = dataset.iloc[:, :-1]
+    labels = dataset.iloc[:, -1]
+    sc = StandardScaler()
+    pca = PCA(n_components=11)
+    data = sc.fit_transform(data)
+    data = pca.fit_transform(data)
+
+    plt.figure(figsize=(10, 10))
+    plt.scatter(data[:, 7], data[:, 8], c=dataset['quality'].apply(lambda x: 'red' if x==1 else 'pink'))
+    plt.title('PCA 8v9')
+    plt.xlabel('Principal Component 8')
+    plt.ylabel('Principal Component 9')
+
+# Function Calls
+Q1()
+Q3()
+Q4()
+Q5()
+Q6()
+Q89()
+
 plt.show()
-
-# Apply t-SNE
-tsne = TSNE(n_components=2, perplexity=30)
-tsne_result = tsne.fit_transform(dataset.drop('quality', axis=1))
-
-# Plot t-SNE results
-plt.figure(figsize=(8, 6))
-plt.scatter(tsne_result[:, 0], tsne_result[:, 1], c=dataset['quality'].apply(map_quality_to_color))
-plt.title('t-SNE')
-plt.xlabel('t-SNE Component 1')
-plt.ylabel('t-SNE Component 2')
-plt.show()
-# Output
-fig.tight_layout()
-plt.show()
-
 
