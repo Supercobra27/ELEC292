1146a04db755276379eecfdf1c0a0099abc75b7b
FosterQU26 <155387690+FosterQU26@users.noreply.github.com>
2024-04-07 19:45:33 -0400

    Update ELEC292_Final.py to add app functionality
    Added GUI
    Still needs tweaks to make_graph and load_file functions

--- FinalProject/ELEC292_Final.py
+++ FinalProject/ELEC292_Final.py
@@ -1,4 +1,4 @@
-# Author(s): Ryan Silverberg
+# Author(s): Ryan Silverberg, Foster Ecklund, Julia Zigelstein
 import pandas as pd
 import os
 import h5py
@@ -11,9 +11,18 @@ from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay,roc_curve,ro
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import recall_score
+import tkinter as tk
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import os
+from tkinter import filedialog
 
 # Global Variables
 scaler = StandardScaler()
+#Global Variables for App
+canvas = None
+csv_out = None
 
 csvs = [
     "RyanWalk.csv",
@@ -62,6 +71,43 @@ def label(csv, label):
     data = pd.read_csv(csv)
     data["label"] = label
     return data
+
+#create and return a graph from a CSV file
+def create_graph(csv_file):
+    #need to add graph for 1/0 walking/jumping
+    df = pd.read_csv(csv_file)
+    fig, ax = plt.subplots()
+    ax.plot(df["Time (s)"], df["Linear Acceleration z (m/s^2)"])
+    ax.set_title("Graph from " + os.path.basename(csv_file))
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Linear Acceleration z (m/s^2)")
+    ax.set_ylim([-75, 75])
+    return fig
+
+# Function to update the displayed graph
+def load_file():
+    global canvas, csv_out
+    csv_in = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+    
+    #call classifier
+    csv_out = csv_in
+
+    if canvas:
+        canvas.get_tk_widget().destroy()  # Destroy the previous canvas
+    fig = create_graph(csv_out)
+    canvas = FigureCanvasTkAgg(fig, master=app)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+
+def save_file():
+    global csv_out
+    save_file = filedialog.asksaveasfilename(defaultextension =".csv", 
+                                            filetypes = [("CSV files", "*.csv")], initialfile = csv_out)
+
+    if save_file:
+        content = pd.read_csv(csv_out)
+        content.to_csv(save_file, index = False)
+        messagebox.showinfo("Success", "File Saved")
 
 for csv in csvs:
     if csv.find("Walk") != -1:
@@ -271,3 +317,17 @@ fpr, tpr, thresholds = roc_curve(Y_test, y_clf_prob[:, 1], pos_label=clf.classes
 roc_display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=auc).plot() # plot ROC
 plt.show()
 # model.predict() # Put DataFrame in here
+
+# Main Tkinter app
+app = tk.Tk()
+app.title("Toggle Graph")
+
+#button loads a file
+load_button = tk.Button(app, text="Load File", command = load_file)
+load_button.pack()
+
+#button saves file
+save_button = tk.Button(app, text = "Save File", command = save_file)
+save_button.pack()
+
+app.mainloop()
